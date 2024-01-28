81bf46245191c8961ad76bd87644eb79ea162262
JosuePimentel <josue.farias.pimentel@gmail.com>
2024-01-28 12:02:26 -0300

    container blog add

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
 document.querySelector(".hamb").addEventListener('click', e => {
-    e.target.parentElement.classList.toggle("show")
+    document.querySelector(".hamb").classList.toggle("show")
     document.querySelector(".menuhamb").classList.toggle("show")
 })
